4288cd1248de5eee24d17b4ef2bc6b12c2b03215
Drew Thompson <drewallynthompson@gmail.com>
2020-09-01 04:09:20 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -12,10 +12,7 @@ def get_the_keys(groceries)
  groceries.keys
 end
 
-def get_the_min(groceries)
-groceries.values.flatten.min
-end
-
 def get_the_values(groceries)
+groceries.values
 groceries.values.flatten
 end
